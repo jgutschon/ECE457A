bfa3134cfea57ee09f9e102ac79daeeae6b7cdaa
Jon Gutschon <jongutschon123@gmail.com>
2021-10-15 14:44:19 -0400

    fix the broken

--- q2.py
+++ q2.py
@@ -29,23 +29,50 @@ maze = [
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
 ]
 
-visited = [[False] * 25] * 25
+# Array to track visited nodes
+visited = [[False for i in range(25)] for j in range(25)]
 
 
 def isValid(y: int, x: int) -> bool:
     # check bounds
-    if y < 0 or x < 0 or y > 24 or x > 24:
+    if y < 0 or x < 0 or y > len(visited) - 1 or x > len(visited) - 1:
         return False
 
     # check for wall
     if maze[y][x] == 1:
         return False
 
-    # check if curr has been visited
+    # ignore repeated states
     if visited[y][x]:
         return False
 
     return True
+
+
+def print_maze() -> None:
+    for row in maze:
+        print(row)
+
+
+def print_visited() -> None:
+    for y in range(len(visited)):
+        for x in range(len(visited)):
+            if visited[y][x]:
+                print("•", end="")
+            elif maze[y][x] == 1:
+                print("█", end="")
+            else:
+                print(" ", end="")
+        print()
+
+
+def count_visited() -> int:
+    count = 0
+    for y in range(len(visited)):
+        for x in range(len(visited)):
+            if visited[y][x]:
+                count += 1
+    return count
 
 
 def bfs_search(start_pos: tuple, end_pos: tuple):
@@ -54,11 +81,16 @@ def bfs_search(start_pos: tuple, end_pos: tuple):
 
     fringe = []
     fringe.append(start_pos)
+    visited[start_pos[0]][start_pos[1]] = True
 
     curr = start_pos
-    while (curr != end_pos) and (len(fringe) != 0):
+    while len(fringe) != 0:
         print(fringe)
         curr = fringe.pop(0)
+        if curr == end_pos:
+            print(f"\nFound goal after exploring {count_visited()} nodes.")
+            # print(f"Final path")
+            return
         curr_y = curr[0]
         curr_x = curr[1]
 
@@ -70,34 +102,19 @@ def bfs_search(start_pos: tuple, end_pos: tuple):
                 fringe.append((y, x))
                 visited[y][x] = True
 
-
-def print_maze():
-    for row in maze:
-        print(row)
-
-
-def print_visited():
-    for y in range(25):
-        for x in range(25):
-            if visited[y][x]:
-                print("•", end="")
-            elif maze[y][x] == 1:
-                print("█", end="")
-            else:
-                print(" ", end="")
-        print()
-
-    # for row in visited:
-    #     print(row)
+    print(f"\Goal not found after exloring {count_visited()} nodes.")
 
 
 if __name__ == "__main__":
     # c.1: Agent starts at S, ends at E1
     # (y, x)
-    start_pos = (11, 2)
-    end_pos = (19, 23)
+    # start_pos = (11, 2)
+    # end_pos = (19, 23)
 
-    bfs_search(start_pos, end_pos)
+    # bfs_search(start_pos, end_pos)
+    # print_visited()
+
+    bfs_search((11, 2), (12, 2))
     print_visited()
 
     # c.2: Agent starts at S, ends at E2
