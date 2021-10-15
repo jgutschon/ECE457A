578b790e9e2a51598536024ad2d28966694d80d1
Jon Gutschon <jongutschon123@gmail.com>
2021-10-15 16:33:44 -0400

    reset state before searching

--- q2.py
+++ q2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# Search directions
+y_dirs = (1, 0, -1, 0)
+x_dirs = (0, 1, 0, -1)
+
 
 class Maze:
     maze = [
@@ -60,6 +64,7 @@ class Maze:
             print(row)
 
     def print_explored(self) -> None:
+        print(f"Explored: •  Maze Wall: █")
         for y in range(len(self.maze)):
             for x in range(len(self.maze)):
                 if (y, x) in self.closed_queue:
@@ -70,22 +75,25 @@ class Maze:
                     print(" ", end="")
             print()
 
+    # Breadth First Search
     def bfs_search(self, start_pos: tuple, end_pos: tuple):
-        # Search directions
-        y_dirs = (1, 0, -1, 0)
-        x_dirs = (0, 1, 0, -1)
+        # Reset queues
+        self.open_queue = []
+        self.closed_queue = []
+        self.expanded = [[False for i in range(25)] for j in range(25)]
 
-        open_queue = []
-        open_queue.append(start_pos)
+        # Add starting position to open_queue
+        self.open_queue.append(start_pos)
         self.expanded[start_pos[0]][start_pos[1]] = True
 
+        # Start searching
         curr = start_pos
-        while len(open_queue) != 0:
-            # print(open_queue)
-            curr = open_queue.pop(0)
+        while len(self.open_queue) != 0:
+            # print(self.open_queue)
+
+            # Add node to closed queue
+            curr = self.open_queue.pop(0)
             self.closed_queue.append(curr)
-            curr_y = curr[0]
-            curr_x = curr[1]
 
             # Check current node for goal
             if curr == end_pos:
@@ -95,10 +103,12 @@ class Maze:
 
             # Expand curr, add adjacent nodes to open_queue
             for i in range(len(x_dirs)):
+                curr_y = curr[0]
+                curr_x = curr[1]
                 y = curr_y + y_dirs[i]
                 x = curr_x + x_dirs[i]
                 if self.isValid(y, x):
-                    open_queue.append((y, x))
+                    self.open_queue.append((y, x))
                     self.expanded[y][x] = True
 
         print(f"Goal not found after exloring {len(self.closed_queue)} nodes.\n")
@@ -108,26 +118,24 @@ if __name__ == "__main__":
     # c.1: Agent starts at S, ends at E1
     start_pos = (11, 2)
     end_pos = (19, 23)
-    print(f"\nc.1 - start, S: {start_pos}, end, E1: {end_pos}\n")
+    print(f"\n\nc.1 - start, S: {start_pos}, end, E1: {end_pos}\n")
 
-    m_c1 = Maze()
-    m_c1.bfs_search(start_pos, end_pos)
-    m_c1.print_explored()
+    m = Maze()
+    m.bfs_search(start_pos, end_pos)
+    m.print_explored()
 
     # c.2: Agent starts at S, ends at E2
     start_pos = (11, 2)
     end_pos = (21, 2)
-    print(f"\nc.2 - start, S: {start_pos}, end, E2: {end_pos}\n")
+    print(f"\n\nc.2 - start, S: {start_pos}, end, E2: {end_pos}\n")
 
-    m_c2 = Maze()
-    m_c2.bfs_search(start_pos, end_pos)
-    m_c2.print_explored()
+    m.bfs_search(start_pos, end_pos)
+    m.print_explored()
 
     # c.3: Agent starts at (0,0), ends at (24,24)
     start_pos = (0, 0)
     end_pos = (24, 24)
-    print(f"\nc.3 - start: {start_pos}, end: {end_pos}\n")
+    print(f"\n\nc.3 - start: {start_pos}, end: {end_pos}\n")
 
-    m_c3 = Maze()
-    m_c3.bfs_search(start_pos, end_pos)
-    m_c3.print_explored()
+    m.bfs_search(start_pos, end_pos)
+    m.print_explored()
