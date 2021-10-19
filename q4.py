ca2e00422c6c1235cc5c438ade2cfe238a922b1b
Jon Gutschon <jongutschon123@gmail.com>
2021-10-19 00:02:17 -0400

    add black

--- q4.py
+++ q4.py
@@ -1,12 +1,16 @@
+from urllib.request import HTTPDigestAuthHandler
+from PIL.Image import init
 import numpy as np
 import numpy.random as rn
-
-# to plot
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 # to compare
 from scipy import optimize
+from scipy.optimize._trustregion_constr.canonical_constraint import (
+    initial_constraints_as_canonical,
+)
+from scipy.sparse.construct import rand
 
 import seaborn as sns
 
@@ -14,102 +18,35 @@ sns.set(
     context="talk", style="darkgrid", palette="hls", font="sans-serif", font_scale=1.05
 )
 
-FIGSIZE = (19, 8)  #: Figure size, in inches!
+# Figure size in inches
+FIGSIZE = (19, 8)
 mpl.rcParams["figure.figsize"] = FIGSIZE
 
+# Bounds
+interval = (-100, 100)
 
-# ## Algorithm
-#
-# The following pseudocode presents the simulated annealing heuristic.
-#
-# - It starts from a state $s_0$ and continues to either a maximum of $k_{\max}$ steps or until a state with an energy of $e_{\min}$ or less is found.
-# - In the process, the call $\mathrm{neighbour}(s)$ should generate a randomly chosen neighbour of a given state $s$.
-# - The annealing schedule is defined by the call $\mathrm{temperature}(r)$, which should yield the temperature to use, given the fraction $r$ of the time budget that has been expended so far.
-
-# > **Simulated Annealing**:
-# >
-# > - Let $s$ = $s_0$
-# > - For $k = 0$ through $k_{\max}$ (exclusive):
-# >    + $T := \mathrm{temperature}(k ∕ k_{\max})$
-# >    + Pick a random neighbour, $s_{\mathrm{new}} := \mathrm{neighbour}(s)$
-# >    + If $P(E(s), E(s_{\mathrm{new}}), T) \geq \mathrm{random}(0, 1)$:
-# >       * $s := s_{\mathrm{new}}$
-# > - Output: the final state $s$
+# function to minimize
+def easom(x1, x2):
+    return (
+        -1
+        * np.cos(x1)
+        * np.cos(x2)
+        * np.exp(-1 * (x1 - np.pi) ** 2 - (x2 - np.pi) ** 2)
+    )
 
 
-# Let us start with a very generic implementation:
+# Temperature Scheduling
+def temp_schedule(t, alpha, rule):
+    if rule == "lin":
+        t -= alpha
+    elif rule == "exp":
+        t *= alpha
+    elif rule == "slow":
+        t /= 1 + alpha
+    else:
+        print("Invalid rule")
 
-
-def annealing(
-    random_start,
-    cost_function,
-    random_neighbour,
-    acceptance,
-    temperature,
-    maxsteps=1000,
-    debug=True,
-):
-    """Optimize the black-box function 'cost_function' with the simulated annealing algorithm."""
-    state = random_start()
-    cost = cost_function(state)
-    states, costs = [state], [cost]
-    for step in range(maxsteps):
-        fraction = step / float(maxsteps)
-        T = temperature(fraction)
-        new_state = random_neighbour(state, fraction)
-        new_cost = cost_function(new_state)
-        if debug:
-            print(
-                "Step #{:>2}/{:>2} : T = {:>4.3g}, state = {:>4.3g}, cost = {:>4.3g}, new_state = {:>4.3g}, new_cost = {:>4.3g} ...".format(
-                    step, maxsteps, T, state, cost, new_state, new_cost
-                )
-            )
-        if acceptance_probability(cost, new_cost, T) > rn.random():
-            state, cost = new_state, new_cost
-            states.append(state)
-            costs.append(cost)
-            # print("  ==> Accept it!")
-        # else:
-        #    print("  ==> Reject it...")
-    return state, cost_function(state), states, costs
-
-
-# ----
-#
-# ## Basic example
-#
-# We will use this to find the global minimum of the function $x \mapsto x^2$ on $[-10, 10]$.
-
-interval = (-10, 10)
-
-
-def f(x):
-    """Function to minimize."""
-    return x ** 2
-
-
-def clip(x):
-    """Force x to be in the interval."""
-    a, b = interval
-    return max(min(x, b), a)
-
-
-def random_start():
-    """Random point in the interval."""
-    a, b = interval
-    return a + (b - a) * rn.random_sample()
-
-
-def cost_function(x):
-    """Cost of x = f(x)."""
-    return f(x)
-
-
-def random_neighbour(x, fraction=1):
-    """Move a little bit x, from the left or the right."""
-    amplitude = (max(interval) - min(interval)) * fraction / 10
-    delta = (-amplitude / 2.0) + amplitude * rn.random_sample()
-    return clip(x + delta)
+    return t
 
 
 def acceptance_probability(cost, new_cost, temperature):
@@ -122,83 +59,62 @@ def acceptance_probability(cost, new_cost, temperature):
         return p
 
 
-def temperature(fraction):
-    """Example of temperature dicreasing as the process goes on."""
-    return max(0.01, min(1, 1 - fraction))
+# b.i. Generate 10 initial points
+def rand_point():
+    rand_points = [[-101 for i in range(2)] for i in range(10)]
+    best_point = [-100, -100]
+    for i in range(10):
+        rand_points[i][0] = np.random.uniform(low=interval[0], high=interval[1], size=1)
+        rand_points[i][1] = np.random.uniform(low=interval[0], high=interval[1], size=1)
+
+        print(rand_points[i])
+        if easom(rand_points[i]) < easom(best_point):
+            best_point = rand_points[i]
+
+    return best_point
 
 
-# Let's try!
+# b.ii. Generate 10 initial temperatures
+def rand_temp():
+    rand_temps = np.random.uniform(low=0, high=10, size=10)
+    best_temp = max(rand_temps)
+    return best_temp
 
 
-annealing(
-    random_start,
-    cost_function,
-    random_neighbour,
-    acceptance_probability,
-    temperature,
-    maxsteps=30,
-    debug=True,
-)
-
-
-# Now with more steps:
-
-
-state, c, states, costs = annealing(
-    random_start,
-    cost_function,
-    random_neighbour,
-    acceptance_probability,
-    temperature,
+# Annealing implementation
+def annealing(
+    start_point: tuple,
+    alpha: float,
+    init_temp,
+    sched_rule,
     maxsteps=1000,
-    debug=False,
-)
+    debug=True,
+):
+    state = start_point
+    cost = easom(state)
+    states, costs = [state], [cost]
+    temp = init_temp
 
-state
-c
+    for step in range(maxsteps):
+        temp = temp_schedule(temp, alpha, sched_rule)
+        new_state = state
+        new_cost = easom(new_state)
 
+        if debug:
+            print(
+                "Step #{:>2}/{:>2} : T = {:>4.3g}, state = {:>4.3g}, cost = {:>4.3g}, new_state = {:>4.3g}, new_cost = {:>4.3g} ...".format(
+                    step, maxsteps, temp, state, cost, new_state, new_cost
+                )
+            )
 
-# Visualizing the steps
-def see_annealing(states, costs):
-    plt.figure()
-    plt.suptitle("Evolution of states and costs of the simulated annealing")
-    plt.subplot(121)
-    plt.plot(states, "r")
-    plt.title("States")
-    plt.subplot(122)
-    plt.plot(costs, "b")
-    plt.title("Costs")
-    plt.show()
-
-
-see_annealing(states, costs)
-
-
-# More visualizations
-def visualize_annealing(cost_function):
-    state, c, states, costs = annealing(
-        random_start,
-        cost_function,
-        random_neighbour,
-        acceptance_probability,
-        temperature,
-        maxsteps=1000,
-        debug=False,
-    )
-    see_annealing(states, costs)
-    return state, c
+        if acceptance_probability(cost, new_cost, temp) > np.random.uniform():
+            state, cost = new_state, new_cost
+            states.append(state)
+            costs.append(cost)
+            # print("  ==> Accept it!")
+        # else:
+        #    print("  ==> Reject it...")
+    return state, easom(state), states, costs
 
 
-visualize_annealing(lambda x: x ** 3)
-
-
-visualize_annealing(lambda x: x ** 2)
-
-
-visualize_annealing(np.abs)
-
-
-visualize_annealing(np.cos)
-
-
-visualize_annealing(lambda x: np.sin(x) + np.cos(x))
+annealing(rand_point(), 0.1, rand_temp(), "lin", 1000)
